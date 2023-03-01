ff0661236221e9de20b5d4785fbd44e6a95682ac
Waterticket <matthew218@naver.com>
2023-03-01 19:35:24 +0900

    feat: initial commit

---
+++ src/test/java/dev/waterticket/jobdemo/JobdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.waterticket.jobdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
