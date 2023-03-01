7d858271c4abf9431bbbdcf9ddee19cd687a224f
Waterticket <matthew218@naver.com>
2023-03-01 23:18:51 +0900

    feat: Index 페이지 구성

---
+++ src/main/java/dev/waterticket/jobdemo/IndexController.java
@@ -0,0 +1,12 @@
+package dev.waterticket.jobdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
