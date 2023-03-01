46f8993d1930b5a8bcc4d18dca60a92081a2bfe2
Waterticket <matthew218@naver.com>
2023-03-02 00:46:38 +0900

    feat: csrf 비활성화 (REST api 한정)

--- src/main/java/dev/waterticket/jobdemo/config/SecurityConfig.java
+++ src/main/java/dev/waterticket/jobdemo/config/SecurityConfig.java
@@ -12,7 +12,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and()
+                .csrf()
+                .disable();
 
         return http.build();
     }
