108ec6ec039611d7af1f903264a5cf17ed2ff1f6
Waterticket <matthew218@naver.com>
2023-03-02 01:27:45 +0900

    feat: 비밀번호 암호화 수단 BCrypt로 설정

--- src/main/java/dev/waterticket/jobdemo/config/SecurityConfig.java
+++ src/main/java/dev/waterticket/jobdemo/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,5 +39,10 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
