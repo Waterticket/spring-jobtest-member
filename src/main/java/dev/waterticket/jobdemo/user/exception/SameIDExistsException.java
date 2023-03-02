30e3ff6a3b0aa3d0f12c60bdf379d41891a50863
Waterticket <matthew218@naver.com>
2023-03-02 22:16:04 +0900

    feat: SameIDExistsException Http response code 409로 변경

--- src/main/java/dev/waterticket/jobdemo/user/exception/SameIDExistsException.java
+++ src/main/java/dev/waterticket/jobdemo/user/exception/SameIDExistsException.java
@@ -3,6 +3,6 @@ package dev.waterticket.jobdemo.user.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Same ID exists.")
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Same ID exists.")
 public class SameIDExistsException extends RuntimeException{
 }
