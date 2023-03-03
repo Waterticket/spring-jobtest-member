f2061c16a4a8923afc486501eddaf9203fc0171f
Waterticket <matthew218@naver.com>
2023-03-03 14:30:56 +0900

    test: Insert, Update Test 케이스 추가

--- src/main/java/dev/waterticket/jobdemo/user/dto/UserUpdateNameRequest.java
+++ src/main/java/dev/waterticket/jobdemo/user/dto/UserUpdateNameRequest.java
@@ -2,9 +2,11 @@ package dev.waterticket.jobdemo.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserUpdateNameRequest {
     @NotBlank
     @Size(min = 4, max = 30, message = "아이디는 4~30자로 입력해주세요")
--- src/test/java/dev/waterticket/jobdemo/UserRESTApiTests.java
+++ src/test/java/dev/waterticket/jobdemo/UserRESTApiTests.java
@@ -1,6 +1,9 @@
 package dev.waterticket.jobdemo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.waterticket.jobdemo.user.domain.User;
+import dev.waterticket.jobdemo.user.dto.UserAddRequest;
+import dev.waterticket.jobdemo.user.dto.UserUpdateNameRequest;
 import dev.waterticket.jobdemo.user.service.UserService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -26,12 +31,16 @@ public class UserRESTApiTests {
     @Autowired
     protected WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public UserRESTApiTests(UserService userService) {
         this.userService = userService;
     }
 
     private final String id = "user_rest_api_tests";
     private final String password = "user_rest_api_tests";
+    private final String invalid_id = "user_rest_api_tests_invalid";
 
     @BeforeAll
     public void addAdminAccount() {
@@ -71,6 +80,78 @@ public class UserRESTApiTests {
         mockMvc.perform(get("/api/user")
                         .header("Authorization", "Basic " + id + ":" + password + "wrong"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void insertAccount_assertCreated() throws Exception {
+        UserAddRequest userAddRequest = UserAddRequest.builder()
+                .id("user_rest_api_tests_insert")
+                .name("user_rest_api_tests_insert")
+                .auth("USER")
+                .build();
+        String content = objectMapper.writeValueAsString(userAddRequest);
+
+        mockMvc.perform(post("/api/user")
+                        .header("Authorization", "Basic " + id + ":" + password)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        this.userService.delete("user_rest_api_tests_insert");
+    }
+
+    @Test
+    public void insertAccountDuplicate_assertConflict() throws Exception {
+        UserAddRequest userAddRequest = UserAddRequest.builder()
+                .id(id)
+                .name("user_rest_api_tests_2")
+                .auth("USER")
+                .build();
+        String content = objectMapper.writeValueAsString(userAddRequest);
+
+        mockMvc.perform(post("/api/user")
+                        .header("Authorization", "Basic " + id + ":" + password)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andDo(print());
+    }
+
+    @Test
+    public void updateAccountName_assertOk() throws Exception {
+        UserUpdateNameRequest userUpdateNameRequest = UserUpdateNameRequest.builder()
+                .id(id)
+                .name(id + "_updated")
+                .build();
+        String content = objectMapper.writeValueAsString(userUpdateNameRequest);
+
+        mockMvc.perform(put("/api/user")
+                        .header("Authorization", "Basic " + id + ":" + password)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void updateAccountNameInvalidUser_assertNotFound() throws Exception {
+        UserUpdateNameRequest userUpdateNameRequest = UserUpdateNameRequest.builder()
+                .id(invalid_id)
+                .name(invalid_id + "_updated")
+                .build();
+        String content = objectMapper.writeValueAsString(userUpdateNameRequest);
+
+        mockMvc.perform(put("/api/user")
+                        .header("Authorization", "Basic " + id + ":" + password)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
     }
 
     @AfterAll
